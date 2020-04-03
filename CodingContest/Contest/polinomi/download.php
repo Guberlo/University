8010ac20998afe248595189a746aa626f041669d
Guberlo <guberlo@guby.station>
2020-04-03 21:05:40 +0200

    Cleaned and restructured

--- CodingContest/Contest/polinomi/download.php
+++
@@ -1,8 +0,0 @@
-<br />
-<b>Warning</b>:  filesize() [<a href='function.filesize'>function.filesize</a>]: stat failed for coding/_input/Guberlo.add-polinomi.1552672071.txt in <b>/home/prog2/public_html/download.php</b> on line <b>7</b><br />
-<br />
-<b>Warning</b>:  Cannot modify header information - headers already sent by (output started at /home/prog2/public_html/download.php:7) in <b>/home/prog2/public_html/download.php</b> on line <b>7</b><br />
-<br />
-<b>Warning</b>:  Cannot modify header information - headers already sent by (output started at /home/prog2/public_html/download.php:7) in <b>/home/prog2/public_html/download.php</b> on line <b>8</b><br />
-<br />
-<b>Warning</b>:  readfile(coding/_input/Guberlo.add-polinomi.1552672071.txt) [<a href='function.readfile'>function.readfile</a>]: failed to open stream: No such file or directory in <b>/home/prog2/public_html/download.php</b> on line <b>11</b><br />
--- python/whilenot.py
+++
@@ -1,5 +0,0 @@
-codice=input("Inserisci codice di accesso: ")
-while codice!="pappa":
-    print("Codice errato, riprova")
-    codice=input("Inserisci codice di accesso: ")
-print("Codice esatto!")
